487739a24a288f0388e4c2c3d9d5e5aaa4d0a7fb
comrade77 <nimec77@gmail.com>
2021-08-30 10:36:13 +0700

    Added widget test for splash_page.dart

--- test/unit/smarteam/app/presentation/blocs/init/init_bloc_test.dart
+++ test/unit/smarteam/app/presentation/blocs/init/init_bloc_test.dart
@@ -20,8 +20,7 @@ void main() {
       'emits [InitState.initSuccess] when event InitEvent.initEnded if state is not InitState.initFailure',
       build: () => InitBloc(smarteam: mockSmarteam),
       act: (initBloc) => initBloc.add(const InitEvent.initEnded()),
-      expect: () =>
-      [
+      expect: () => [
         InitState.initSuccess(mockSmarteam),
       ],
     );
@@ -30,58 +29,60 @@ void main() {
       'emits [InitState.initTimeout] when event InitEvent.initTimeUp',
       build: () => InitBloc(smarteam: mockSmarteam),
       act: (initBloc) => initBloc.add(const InitEvent.initTimeUp()),
-      expect: () =>
-      [
+      expect: () => [
         const InitState.initTimeout(),
       ],
     );
 
     blocTest<InitBloc, InitState>(
-      'emits [InitState.initInProgress(0.1), InitState.initInProgress(1 - 1 / kLoadDuration.inSeconds)]'
-          ' when event InitEvent.initStarted Right(true)',
-      build: () {
-        when(mockSmarteam.init).thenAnswer((_) => Future.value(const Right<Error, bool>(true)));
+        'emits [InitState.initInProgress(0.1), InitState.initInProgress(1 - 1 / kLoadDuration.inSeconds)]'
+        ' when event InitEvent.initStarted Right(true)',
+        build: () {
+          when(mockSmarteam.init).thenAnswer((_) => Future.value(const Right<Error, bool>(true)));
 
-        return InitBloc(smarteam: mockSmarteam);
-      },
-      act: (initBloc) => initBloc.add(const InitEvent.initStarted()),
-      expect: () =>
-      [
-        const InitState.initInProgress(0.1),
-        InitState.initInProgress(1 - 1 / kLoadDuration.inSeconds),
-      ],
-    );
-
-    blocTest<InitBloc, InitState>(
-      "emits [InitState.initInProgress(0.1), InitState.initFailure(SmarteamError('Smarteam initialization error')]"
-          ' when event InitEvent.initStarted Right(false)',
-      build: () {
-        when(mockSmarteam.init).thenAnswer((_) => Future.value(const Right<Error, bool>(false)));
-
-        return InitBloc(smarteam: mockSmarteam);
-      },
-      act: (initBloc) => initBloc.add(const InitEvent.initStarted()),
-      expect: () =>
-      [
-        const InitState.initInProgress(0.1),
-        InitState.initFailure(SmarteamError('Smarteam initialization error')),
-      ],
-    );
+          return InitBloc(smarteam: mockSmarteam);
+        },
+        act: (initBloc) => initBloc.add(const InitEvent.initStarted()),
+        expect: () => [
+              const InitState.initInProgress(0.1),
+              InitState.initInProgress(1 - 1 / kLoadDuration.inSeconds),
+            ],
+        verify: (_) {
+          verify(mockSmarteam.init).called(1);
+        });
 
     blocTest<InitBloc, InitState>(
-      "emits [InitState.initInProgress(0.1), InitState.initFailure(SmarteamError('Error')]"
-          ' when event InitEvent.initStarted Left(SmarteamError)',
-      build: () {
-        when(mockSmarteam.init).thenAnswer((_) => Future.value(Left(SmarteamError('Error'))));
+        "emits [InitState.initInProgress(0.1), InitState.initFailure(SmarteamError('Smarteam initialization error')]"
+        ' when event InitEvent.initStarted Right(false)',
+        build: () {
+          when(mockSmarteam.init).thenAnswer((_) => Future.value(const Right<Error, bool>(false)));
 
-        return InitBloc(smarteam: mockSmarteam);
-      },
-      act: (initBloc) => initBloc.add(const InitEvent.initStarted()),
-      expect: () =>
-      [
-        const InitState.initInProgress(0.1),
-        InitState.initFailure(SmarteamError('Error')),
-      ],
-    );
+          return InitBloc(smarteam: mockSmarteam);
+        },
+        act: (initBloc) => initBloc.add(const InitEvent.initStarted()),
+        expect: () => [
+              const InitState.initInProgress(0.1),
+              InitState.initFailure(SmarteamError('Smarteam initialization error')),
+            ],
+        verify: (_) {
+          verify(mockSmarteam.init).called(1);
+        });
+
+    blocTest<InitBloc, InitState>(
+        "emits [InitState.initInProgress(0.1), InitState.initFailure(SmarteamError('Error')]"
+        ' when event InitEvent.initStarted Left(SmarteamError)',
+        build: () {
+          when(mockSmarteam.init).thenAnswer((_) => Future.value(Left(SmarteamError('Error'))));
+
+          return InitBloc(smarteam: mockSmarteam);
+        },
+        act: (initBloc) => initBloc.add(const InitEvent.initStarted()),
+        expect: () => [
+              const InitState.initInProgress(0.1),
+              InitState.initFailure(SmarteamError('Error')),
+            ],
+        verify: (_) {
+          verify(mockSmarteam.init).called(1);
+        });
   });
 }
--- test/widget/smarteam/app/presentation/pages/splash/splash_page_test.dart
+++ test/widget/smarteam/app/presentation/pages/splash/splash_page_test.dart
@@ -1,12 +1,97 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:dart_smarteam/smarteam.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_smarteam/l10n/l10n.dart';
 import 'package:flutter_smarteam/smarteam/app/presentation/blocs/init/init_bloc.dart';
+import 'package:flutter_smarteam/smarteam/app/presentation/pages/splash/splash_page.dart';
+import 'package:flutter_smarteam/smarteam/app/presentation/widgets/show_error_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
 import '../../../../../../helpers/helpers.dart';
 
 class MockInitBloc extends MockBloc<InitEvent, InitState> implements InitBloc {}
 
 void main() {
-  final mockInitBloc = MockInitBloc();
+  late final MockInitBloc mockInitBloc;
 
+  setUpAll(() {
+    registerFallbackValue<InitState>(const InitState.notInit());
+    registerFallbackValue<InitEvent>(const InitEvent.initStarted());
+    mockInitBloc = MockInitBloc();
+  });
+
+  group('SplashPage test', () {
+    testWidgets('SplashPage render test', (widgetTester) async {
+      whenListen<InitState>(
+        mockInitBloc,
+        Stream.fromIterable([const InitState.initInProgress(0.1)]),
+        initialState: const InitState.notInit(),
+      );
+      late final AppLocalizations l10n;
+      final widget = Builder(
+        builder: (context) {
+          l10n = context.l10n;
+          return BlocProvider<InitBloc>.value(
+            value: mockInitBloc,
+            child: const SplashPage(),
+          );
+        },
+      );
+
+      await widgetTester.pumpSizerAndScaffold(widget);
+
+      final textFinder = find.text(l10n.smarteamSplashText);
+      expect(textFinder, findsOneWidget);
+
+      final shaderMaskFinder = find.byType(ShaderMask);
+      expect(shaderMaskFinder, findsOneWidget);
+
+      final animatedOpacityFinder = find.byType(AnimatedOpacity);
+      expect(animatedOpacityFinder, findsOneWidget);
+    });
+
+    testWidgets('SplashPage loading test', (widgetTester) async {
+      whenListen<InitState>(
+        mockInitBloc,
+        Stream.fromIterable([const InitState.initInProgress(0.1)]),
+        initialState: const InitState.notInit(),
+      );
+      final widget = BlocProvider<InitBloc>.value(
+        value: mockInitBloc,
+        child: const SplashPage(),
+      );
+
+      await widgetTester.pumpSizerAndScaffold(widget);
+
+      final animatedOpacityFinder = find.byType(AnimatedOpacity);
+      final animatedOpacityWidget = widgetTester.widget<AnimatedOpacity>(animatedOpacityFinder);
+      expect(animatedOpacityWidget.opacity, 0.0);
+    });
+
+    testWidgets('SplashPage error initialization', (widgetTester) async {
+      final error = SmarteamError('Initialization error');
+      whenListen<InitState>(
+        mockInitBloc,
+        Stream.fromIterable([InitState.initFailure(error)]),
+        initialState: const InitState.notInit(),
+      );
+      final widget = BlocProvider<InitBloc>.value(
+        value: mockInitBloc,
+        child: const SplashPage(),
+      );
+
+      await widgetTester.pumpSizerAndScaffold(widget);
+      await widgetTester.pumpAndSettle();
+      final animatedOpacityFinder = find.byType(AnimatedOpacity);
+      final animatedOpacityWidget = widgetTester.widget<AnimatedOpacity>(animatedOpacityFinder);
+      expect(animatedOpacityWidget.opacity, 1.0);
+
+      final showErrorFinder = find.byType(ShowErrorWidget);
+      expect(showErrorFinder, findsOneWidget);
+      final showErrorWidget = widgetTester.widget<ShowErrorWidget>(showErrorFinder);
+      expect(showErrorWidget.errorMessage, equals(error.toString()));
+    });
+  });
 }
